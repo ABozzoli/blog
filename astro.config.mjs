991ecf5fb1f557ab84bb55a19aac7ceeaf734d2f
ABozzoli <alessandrobozzoli92@gmail.com>
2025-10-01 01:13:48 +0200

    fix: fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://abozzoli.github.io',
-  base: '/abozzoli-blog',
+  base: '/abozzoli-blog/',
 });
