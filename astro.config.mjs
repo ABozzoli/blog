22834e132848d6ee7d316c9133f5b6063f441609
ABozzoli <alessandrobozzoli92@gmail.com>
2025-10-21 01:36:50 +0200

    chore(deps): add astro-icon package, fix warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://abozzoli.github.io',
   base: '/blog/',
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
 });
